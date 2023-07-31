54705f16669c8f113dd082f2f4cf818e73ef0f30
leighannemarais <leighannemarais@gmail.com>
2023-07-31 14:02:52 +0200

    Added weather content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./index.css";
+import Weather from "./Weather";
+import "./App.css";
 import SearchEngine from "./SearchEngine";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <Weather />
       <SearchEngine />
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
   let weatherData = {
@@ -11,27 +12,60 @@ export default function Weather() {
     wind: "30 km/h",
     temp: "19",
   };
+  function SearchEngine() {
+    const [city, setCity] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [weather, setWeather] = useState({});
+
+    function displayWeather(response) {
+      setLoaded(true);
+      setWeather({
+        temperature: response.data.main.temp,
+        wind: response.data.wind.speed,
+        humidity: response.data.main.humidity,
+        icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+        description: response.data.weather[0].description,
+      });
+    }
+
+    function handleSubmit(event) {
+      event.preventDefault();
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=502dc8f7ae36e57af1974e18d16a86f8&units=metric`;
+      axios.get(apiUrl).then(displayWeather);
+    }
+
+    function updateCity(event) {
+      setCity(event.target.value);
+    }
+
+    let form = (
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={updateCity} />
+        <button type="Submit">Search</button>
+      </form>
+    );
+
+    if (loaded) {
+      return (
+        <div>
+          {form}
+          <ul>
+            <li>Temperature: {Math.round(weather.temperature)}°C</li>
+            <li>Description: {weather.description}</li>
+            <li>Humidity: {weather.humidity}%</li>
+            <li>Wind: {weather.wind}km/h</li>
+            <li>
+              <img src={weather.icon} alt="Weather Icon" />
+            </li>
+          </ul>
+        </div>
+      );
+    } else {
+      return form;
+    }
+  }
   return (
     <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search city.."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
       <div className="overview">
         <h1>{weatherData.city}</h1>
 
@@ -44,10 +78,7 @@ export default function Weather() {
             <img src={weatherData.imgUrl} alt="Clear" />
             <div>
               <strong>{weatherData.temp}</strong>
-              <span className="units">
-                {" "}
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
+              <span className="units"> °C</span>
             </div>
           </div>
         </div>
